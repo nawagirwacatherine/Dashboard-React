e68216bc8e99715d485d76f36cd868623674131b
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-04-29 10:11:29 +0200

    remove sidebar file

--- react-dashboard/src/Parts.jsx
+++ react-dashboard/src/Parts.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import "./Parts.css"
 import LeftPart from './parts/LeftPart'
 import Searchpart from './parts/Searchpart'
 import ContentPart from './parts/ContentPart'
@@ -8,10 +9,14 @@ import ContentPart from './parts/ContentPart'
 
 const Parts = () => {
   return (
-    <div>
-        <LeftPart/>
-        <Searchpart />
-        <ContentPart/>
+    <div className='parts'>
+
+        <div className='left-part'><LeftPart/></div>
+        
+        <div className='search-bar'><Searchpart /></div>
+        
+        <div className='content-part'><ContentPart/></div>
+        
     </div>
   )
 }
