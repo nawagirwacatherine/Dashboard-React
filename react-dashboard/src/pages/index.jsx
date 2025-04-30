8735eb091f699a321cdeb9b31252812556dff899
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-04-30 05:57:41 +0200

    Add pages and export all of them via one file indexjs

---
+++ react-dashboard/src/pages/index.jsx
@@ -0,0 +1,17 @@
+
+export { default as Ecommerce } from './Charts/Ecommerce';
+export { default as Kanban } from './Charts/Kanban';
+export { default as Orders } from './Charts/Orders';
+export { default as Employees } from './Charts/Employees';
+export { default as Editor } from './Charts/Editor';
+export { default as Customers } from './Charts/Customer';
+export { default as ColorPicker } from './Charts/ColorPick';
+export { default as Calender } from './Charts/Calender';
+export { default as Area } from './Charts/Area';
+export { default as Bar } from './Charts/Bar';
+export { default as ColorMapping } from './Charts/ ColorMappin';
+export { default as Financial } from './Charts/Financial';
+export { default as Line } from './Charts/Line';
+export { default as Pie} from './Charts/Pie';
+export { default as Pyramid } from './Charts/Pyramid';
+export { default as Stacked } from './Charts/Stacked';
