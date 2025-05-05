d80db705651b9cbce7e5b2e04cb8264a83739327
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-05-05 10:56:25 +0200

    make correction on file importation

--- react-dashboard/src/pages/index.jsx
+++ react-dashboard/src/pages/index.jsx
@@ -1,15 +1,15 @@
 
-export { default as Ecommerce } from './Charts/Ecommerce';
-export { default as Kanban } from './Charts/Kanban';
+export { default as Ecommerce } from './Ecommerce';
+export { default as Kanban } from './Kanban';
 export { default as Orders } from './Charts/Orders';
-export { default as Employees } from './Charts/Employees';
-export { default as Editor } from './Charts/Editor';
-export { default as Customers } from './Charts/Customer';
-export { default as ColorPicker } from './Charts/ColorPick';
-export { default as Calender } from './Charts/Calender';
+export { default as Employees } from './Employees';
+export { default as Editor } from './Editor';
+export { default as Customers } from './Customers';
+export { default as ColorPicker } from './ColorPicker';
+export { default as Calender } from './Calender';
 export { default as Area } from './Charts/Area';
 export { default as Bar } from './Charts/Bar';
-export { default as ColorMapping } from './Charts/ ColorMappin';
+export { default as ColorMapping } from './Charts/ColorMapping';
 export { default as Financial } from './Charts/Financial';
 export { default as Line } from './Charts/Line';
 export { default as Pie} from './Charts/Pie';
--- react-dashboard/src/parts/LeftPart.jsx
+++ react-dashboard/src/parts/LeftPart.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { Navbar, Footer, Sidebar, ThemeSettings} from './components'
-import { Ecommerce, Orders, Calender, Employees, Stacked, Pyramid, Customers, Kanban, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor } from './pages';
+import { Orders, Stacked, Pyramid, Area, Bar, Pie, Financial,ColorMapping,Line } from '../pages';
 
 const LeftPart = () => {
   return (
@@ -10,30 +9,30 @@ const LeftPart = () => {
         <Routes>
           
            {/* Dashboard */}
-           <Route path="/" element="Ecommerce"/>
-           <Route path="./ecommerce" element="./Ecommerce" />
+           <Route path="/" element={<Ecommerce />}/>
+           <Route path="/ecommerce" element={<Ecommerce/>} />
 
            {/* pages */}
-           <Route path="./orders" element="./Orders" />
-           <Route path="./employees" elements="./Employees" />
-           <Route path="./customers" element="./Customers" />
+           <Route path="/orders" element={<Orders/>}/>
+           <Route path="/employees" elements={<Employees/>} />
+           <Route path="/customers" element={<Customers />}/>
 
            {/* Apps */}
 
-           <Route path="./kanban" element="./Kanban" />
-           <Route  path="./editor" element="./Editor"/>
-           <Route path="./calender" element="./Calender"/>
-           <Route path="./color-picker" element="./ColorPicker" />
+           <Route path="/kanban" element={<Kanban/>} />
+           <Route  path="/editor" element={<Editor/>}/>
+           <Route path="/calender" element={<Calender/>}/>
+           <Route path="/color-picker" element={<ColorPicker/>}/>
 
            {/* charts */}
-           <Route path="./line" element="./Line" />
-           <Route path="./area" element="./Area" />
-           <Route path="./bar" element="./Bar" />
-           <Route path="./pie" element="./Pie" />
-           <Route path="./financial" element="./Financial" />
-           <Route path="./color-mapping" element="./ColorMapping" />
-           <Route path="./pyramid" element="./Pyramid" />
-           <Route path="./stacked" element="./Stacked" />
+           <Route path="/line" element={<Line/>}/>
+           <Route path="/area" element={<Area/>} />
+           <Route path="/bar" element={<Bar/>} />
+           <Route path="/pie" element={<Pie/>}/>
+           <Route path="/financial" element={<Financial/>} />
+           <Route path="/color-mapping" element={<ColorMapping/>} />
+           <Route path="/pyramid" element={<Pyramid/>} />
+           <Route path="/stacked" element={<Stacked/>}/>
 
            
          
