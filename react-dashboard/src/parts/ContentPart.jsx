4db7e4413522d21af76eaffe7f14e4bcc7523ce3
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-05-05 11:34:00 +0200

    Add functionality to contents page

--- react-dashboard/src/parts/ContentPart.jsx
+++ react-dashboard/src/parts/ContentPart.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import './content.css'
+
+import { BrowserRouter as Router} from 'react-router-dom'
 import {FiSettings  } from "react-icons/fi";
 
 const ContentPart = () => {
   return (
     <>
     <div>
-    content part
+    <Router>
     <div className='settings'>
     
       <button className='settings-button'><FiSettings /></button>
       </div>
+      </Router>
       </div>
     </>
   )
