3d3910fa93ce0a2b0392d7903cf52b4411a51a5d
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-05-06 06:47:12 +0200

    Add react tooltip

--- react-dashboard/src/components/Sidebar.jsx
+++ react-dashboard/src/components/Sidebar.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { Link, NavLink } from 'react-router-dom';
 import { SiShopware } from  'react-icons/si';
 import { MdOutlineCancel } from 'react-icons/md';
+import ReactTooltip from 'react-tooltip';
 import { links } from '../data/dummy';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='side-bar'>Sidebar</div>
   )
 }
 
