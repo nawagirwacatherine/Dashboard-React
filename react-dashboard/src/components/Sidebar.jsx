389d356a1c2bae977557c863b65c0ac497da4587
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-05-08 00:00:48 +0200

    add tooltip to the closing button

--- react-dashboard/src/components/Sidebar.jsx
+++ react-dashboard/src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import { Link, NavLink } from 'react-router-dom';
 import { SiShopware } from  'react-icons/si';
 import { MdOutlineCancel } from 'react-icons/md';
 import { Tooltip } from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css'; 
 // import { links } from '../data/dummy';
 
 const Sidebar = () => {
@@ -12,11 +13,24 @@ const Sidebar = () => {
 
       {activeMenu && (<>
       <div className='items'>
-        <Link to="/" onClick={() => {}} className='shopware' data-tip="Go to dashboard">
+        <Link to="/" onClick={() => {}} className='shopware' data-tooltip-id="dashboard-tooltip">
        < SiShopware /> <span>Shoppy</span> 
         </Link>
 
-        <Tooltip id="my-tooltip" />
+        <Tooltip id="dashboard-tooltip" 
+        render={() => (
+          <button onClick={() => alert('clicked!')}
+          
+          style={{
+            background: 'transparent',
+            border: 'none',
+            color: 'red',
+            fontSize: '1.2rem',
+            cursor: 'pointer'
+            
+          }}><MdOutlineCancel/></button>
+        )}
+        />
       </div>
       </>)}
     </div>
