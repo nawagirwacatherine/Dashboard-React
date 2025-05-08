dd13ab2566cf27165b921e15b448560be1b7b36a
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-05-08 07:16:01 +0200

    change the router and routes to the app.jsx file

--- react-dashboard/src/parts/ContentPart.jsx
+++ react-dashboard/src/parts/ContentPart.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import './content.css'
-
+import { Outlet } from 'react-router-dom';
 import { BrowserRouter as Router} from 'react-router-dom'
 import {FiSettings  } from "react-icons/fi";
 
 const ContentPart = () => {
   return (
     <>
+
+<div className="content-display">
+      <Outlet /> {/* This is where your pages like Ecommerce, Orders, etc. will render */}
+    </div>
     <div>
-    <Router>
+ 
     <div className='settings'>
     
       <button className='settings-button'><FiSettings /></button>
       </div>
-      </Router>
       </div>
     </>
   )
--- react-dashboard/src/parts/LeftPart.jsx
+++ react-dashboard/src/parts/LeftPart.jsx
@@ -1,54 +1,15 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { Orders, Stacked, Pyramid, Area, Bar, Pie, Financial,ColorMapping,Line } from '../pages';
-import {Ecommerce, Calender, ColorPicker, Customers, Editor, Employees, Kanban} from '../pages';
 import Sidebar from "../components/Sidebar.jsx";
 
 const LeftPart = () => {
   return (
-    <div>
-      <Router>
+   
 
         <div>
           < Sidebar />
         </div>
-        <div className="left-part">
-          
-        </div>
+       
 
-        <Routes>
-          
-           {/* Dashboard */}
-           <Route path="/" element={<Ecommerce />}/>
-           <Route path="/ecommerce" element={<Ecommerce/>} />
-
-           {/* pages */}
-           <Route path="/orders" element={<Orders/>}/>
-           <Route path="/employees" elements={<Employees/>} />
-           <Route path="/customers" element={<Customers />}/>
-
-           {/* Apps */}
-
-           <Route path="/kanban" element={<Kanban/>} />
-           <Route  path="/editor" element={<Editor/>}/>
-           <Route path="/calender" element={<Calender/>}/>
-           <Route path="/color-picker" element={<ColorPicker/>}/>
-
-           {/* charts */}
-           <Route path="/line" element={<Line/>}/>
-           <Route path="/area" element={<Area/>} />
-           <Route path="/bar" element={<Bar/>} />
-           <Route path="/pie" element={<Pie/>}/>
-           <Route path="/financial" element={<Financial/>} />
-           <Route path="/color-mapping" element={<ColorMapping/>} />
-           <Route path="/pyramid" element={<Pyramid/>} />
-           <Route path="/stacked" element={<Stacked/>}/>
-
-           
-         
-        </Routes>
-      </Router>
-    </div>
   )
 }
 
