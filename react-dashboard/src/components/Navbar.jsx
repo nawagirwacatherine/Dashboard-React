b1fa75d900e0da1a3b316efd6925243267b133d3
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-05-19 20:22:50 +0200

    Add ui components to the Navbar component

--- react-dashboard/src/components/Navbar.jsx
+++ react-dashboard/src/components/Navbar.jsx
@@ -1,4 +1,18 @@
 import React from 'react'
+import { AiOutlineMenu } from 'react-icons/ai';
+import { FiShoppingCart } from 'react-icons/fi';
+import { BsChatLeft } from 'react-icons/bs';
+import { RiNotification3Line } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+
+import 'react-tooltip/dist/react-tooltip.css';
+import  avatar from '../data/avatar.jpg'
+import { Cart, Chat, Notification, UserProfile } from '.'
+import { useStateContext } from '../contexts/ContextProvider';
+
+
+
+
 
 const Navbar = () => {
   return (
