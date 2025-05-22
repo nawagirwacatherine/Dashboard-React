aec4360daa777801db64c5e98c6888e53061a69d
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-05-22 02:08:52 +0200

    Add different components in dummy file

--- react-dashboard/src/data/dummy.jsx
+++ react-dashboard/src/data/dummy.jsx
@@ -119,3 +119,423 @@ links:[
 ]
 
 
+
+export  const links = [
+{
+    title:'Dashboard',
+    links: [
+        {
+            name:'ecommerce',
+            icon:< FaCreativeCommonsSampling />,
+        },
+    ]
+},
+
+{
+title: 'pages',
+links:[
+    {
+        name: 'orders',
+        icon: < BsBorderStyle />,
+    },
+
+    {
+     name: 'employees',
+     icon:< BsPeopleFill />,
+    },
+
+    {
+        name:'customers',
+        icon:<RiCustomerService2Line />,
+    },
+],
+},
+
+
+{
+    title:'Apps',
+    links: [
+        {
+            name: 'Kanban',
+            icon:< MdOutlineViewKanban />,
+        },
+        {
+            name: 'calender',
+            icon:< SlCalender />,
+        },
+        {
+            name: 'editor',
+            icon:< SiCkeditor4  />,
+        },
+        {
+            name: 'ColorPicker',
+            icon:< TbColorPicker />,
+        },
+    ],
+},
+
+{
+    title:'charts',
+    links:[
+        {
+            name:'line',
+            icon: < FaLine />
+
+        },
+        {
+            name:'area',
+            icon: < MdOutlineAreaChart />
+
+        },
+        {
+            name:'bar',
+            icon: <  FaBarcode  />
+
+        },
+        {
+            name:'pie',
+            icon: <  BiSolidPieChartAlt />
+
+        },
+        {
+            name:'financial',
+            icon: < GrCurrency  />
+
+        },
+        {
+            name:'colormapping',
+            icon: <  IoColorPalette />
+
+        },
+        {
+            name:'pyramid',
+            icon: < GiGreatPyramid/>
+
+        },
+        {
+            name:'stacked',
+            icon: < IoFileTrayStacked  />
+
+        },
+
+    ]
+}
+]
+
+
+
+
+export  const earningData = [
+{
+    title:'Dashboard',
+    links: [
+        {
+            name:'ecommerce',
+            icon:< FaCreativeCommonsSampling />,
+        },
+    ]
+},
+
+{
+title: 'pages',
+links:[
+    {
+        name: 'orders',
+        icon: < BsBorderStyle />,
+    },
+
+    {
+     name: 'employees',
+     icon:< BsPeopleFill />,
+    },
+
+    {
+        name:'customers',
+        icon:<RiCustomerService2Line />,
+    },
+],
+},
+
+
+{
+    title:'Apps',
+    links: [
+        {
+            name: 'Kanban',
+            icon:< MdOutlineViewKanban />,
+        },
+        {
+            name: 'calender',
+            icon:< SlCalender />,
+        },
+        {
+            name: 'editor',
+            icon:< SiCkeditor4  />,
+        },
+        {
+            name: 'ColorPicker',
+            icon:< TbColorPicker />,
+        },
+    ],
+},
+
+{
+    title:'charts',
+    links:[
+        {
+            name:'line',
+            icon: < FaLine />
+
+        },
+        {
+            name:'area',
+            icon: < MdOutlineAreaChart />
+
+        },
+        {
+            name:'bar',
+            icon: <  FaBarcode  />
+
+        },
+        {
+            name:'pie',
+            icon: <  BiSolidPieChartAlt />
+
+        },
+        {
+            name:'financial',
+            icon: < GrCurrency  />
+
+        },
+        {
+            name:'colormapping',
+            icon: <  IoColorPalette />
+
+        },
+        {
+            name:'pyramid',
+            icon: < GiGreatPyramid/>
+
+        },
+        {
+            name:'stacked',
+            icon: < IoFileTrayStacked  />
+
+        },
+
+    ]
+}
+]
+
+
+
+
+export  const SparklineAreaData = [
+{
+    title:'Dashboard',
+    links: [
+        {
+            name:'ecommerce',
+            icon:< FaCreativeCommonsSampling />,
+        },
+    ]
+},
+
+{
+title: 'pages',
+links:[
+    {
+        name: 'orders',
+        icon: < BsBorderStyle />,
+    },
+
+    {
+     name: 'employees',
+     icon:< BsPeopleFill />,
+    },
+
+    {
+        name:'customers',
+        icon:<RiCustomerService2Line />,
+    },
+],
+},
+
+
+{
+    title:'Apps',
+    links: [
+        {
+            name: 'Kanban',
+            icon:< MdOutlineViewKanban />,
+        },
+        {
+            name: 'calender',
+            icon:< SlCalender />,
+        },
+        {
+            name: 'editor',
+            icon:< SiCkeditor4  />,
+        },
+        {
+            name: 'ColorPicker',
+            icon:< TbColorPicker />,
+        },
+    ],
+},
+
+{
+    title:'charts',
+    links:[
+        {
+            name:'line',
+            icon: < FaLine />
+
+        },
+        {
+            name:'area',
+            icon: < MdOutlineAreaChart />
+
+        },
+        {
+            name:'bar',
+            icon: <  FaBarcode  />
+
+        },
+        {
+            name:'pie',
+            icon: <  BiSolidPieChartAlt />
+
+        },
+        {
+            name:'financial',
+            icon: < GrCurrency  />
+
+        },
+        {
+            name:'colormapping',
+            icon: <  IoColorPalette />
+
+        },
+        {
+            name:'pyramid',
+            icon: < GiGreatPyramid/>
+
+        },
+        {
+            name:'stacked',
+            icon: < IoFileTrayStacked  />
+
+        },
+
+    ]
+}
+]
+
+
+
+// ecomPieChartDat
+
+export  const ecomPieChartDat = [
+{
+    title:'Dashboard',
+    links: [
+        {
+            name:'ecommerce',
+            icon:< FaCreativeCommonsSampling />,
+        },
+    ]
+},
+
+{
+title: 'pages',
+links:[
+    {
+        name: 'orders',
+        icon: < BsBorderStyle />,
+    },
+
+    {
+     name: 'employees',
+     icon:< BsPeopleFill />,
+    },
+
+    {
+        name:'customers',
+        icon:<RiCustomerService2Line />,
+    },
+],
+},
+
+
+{
+    title:'Apps',
+    links: [
+        {
+            name: 'Kanban',
+            icon:< MdOutlineViewKanban />,
+        },
+        {
+            name: 'calender',
+            icon:< SlCalender />,
+        },
+        {
+            name: 'editor',
+            icon:< SiCkeditor4  />,
+        },
+        {
+            name: 'ColorPicker',
+            icon:< TbColorPicker />,
+        },
+    ],
+},
+
+{
+    title:'charts',
+    links:[
+        {
+            name:'line',
+            icon: < FaLine />
+
+        },
+        {
+            name:'area',
+            icon: < MdOutlineAreaChart />
+
+        },
+        {
+            name:'bar',
+            icon: <  FaBarcode  />
+
+        },
+        {
+            name:'pie',
+            icon: <  BiSolidPieChartAlt />
+
+        },
+        {
+            name:'financial',
+            icon: < GrCurrency  />
+
+        },
+        {
+            name:'colormapping',
+            icon: <  IoColorPalette />
+
+        },
+        {
+            name:'pyramid',
+            icon: < GiGreatPyramid/>
+
+        },
+        {
+            name:'stacked',
+            icon: < IoFileTrayStacked  />
+
+        },
+
+    ]
+}
+]
+
+
--- react-dashboard/src/pages/Ecommerce.jsx
+++ react-dashboard/src/pages/Ecommerce.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BsCurrencyDollar } from 'react-icons/bs';
 
-import { GoPrimitiveDot } from 'react-icons/go';
+import { GoDot } from 'react-icons/go';
 
 import { Stacked, Pie, Button,SparkLine } from '../components';
 
