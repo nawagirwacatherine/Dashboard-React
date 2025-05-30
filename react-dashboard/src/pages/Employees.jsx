32cc34391bc4ad7e35270ed19c9425787b1019e2
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-05-30 07:41:06 +0200

    Add content in employees

--- react-dashboard/src/pages/Employees.jsx
+++ react-dashboard/src/pages/Employees.jsx
@@ -1,13 +1,63 @@
-import React from 'react'
-// import { Calendar } from "../components/ui/calendar"
+// import React from 'react'
+
+
+// const Employees = () => {
+//   return (
+//     <div className='min-h-screen bg-gray-50'>
+//      <h1 className='text-2xl font-bold mb-4 p-4 '>Employee Calendar</h1>
+      
+//     </div>
+//   )
+// }
+
+// export default Employees
+
+
+
+
+import React from 'react';
+import {
+  GridComponent,
+  ColumnsDirective,
+  ColumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject
+} from '@syncfusion/ej2-react-grids';
+
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Employees = () => {
   return (
-    <div className='min-h-screen bg-gray-50'>
-     <h1 className='text-2xl font-bold mb-4 p-4 '>Employee Calendar</h1>
-      {/* <Calendar/> */}
-    </div>
-  )
-}
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Orders' />
 
-export default Employees
+      <GridComponent
+        id='gridcomp'
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+        contextMenuItems={contextMenuItems}
+        allowExcelExport
+        allowPdfExport
+        editSettings={{ allowEditing: true, allowDeleting: true, allowAdding: true }}
+      >
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport]} />
+      </GridComponent>
+    </div>
+  );
+};
+
+export default Employees ;
