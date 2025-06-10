d65d43f57f7cabce5c48683f762d059f54756613
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-06-10 10:34:23 +0200

    remove unused divs in ecommerce

--- react-dashboard/src/pages/Ecommerce.jsx
+++ react-dashboard/src/pages/Ecommerce.jsx
@@ -3,39 +3,16 @@ import { BsCurrencyDollar } from 'react-icons/bs';
 
 import { GoDot } from 'react-icons/go';
 
-import { Pie, Button, SparkLine } from '../components';
+import { Button, SparkLine } from '../components';
 
-import { earningData, SparklineAreaData, ecomPieChartDat} from '../data/dummy';
+import { earningData, SparklineAreaData,} from '../data/dummy';
 
-import { useStateContext } from '../contexts/ContextProvider';
 
 const Ecommerce = () => {
   return (
-    <div className="m-20 md:m-10 ">
+    <div className="m-4 md:m-10">
       <div className='flex flex-wrap lg:flex-nowrap justify-center'>
-        <div className='bg-white 
-        dark:text-gray-200
-        dark:bg-secondary-dark-bg h-44
-        rounded-xl w-full lg:w-80 p-8
-        pt-9 m-3 bg-hero-pattern bg-no-repeat bg-cover bg-center'
-        >
-     
-          <div className='flex justify-between items-center'>
-            <p font-bold text-gray-400>Earnings</p>
-            <p className='text-2xl'>$63,448.78</p>
 
-          </div>
-          
-        </div>
-   <div className='mt-6'>
-         <Button 
-         color='white'
-         bgColor='blue'
-         text='Download'
-         borderRadius='10px'
-         size ='md'
-         />
-        </div>
 
         <div className='flex m-3 flex-wrap justify-center gap-1 items-center'>
           {earningData.map((item) =>
@@ -66,7 +43,17 @@ const Ecommerce = () => {
              </p>
 
              <p className='text-sm text-gray-400 mt-1'>{item.title}</p>
+                <div className='mt-6'>
+         <Button 
+         color='white'
+         bgColor='blue'
+         text='Download'
+         borderRadius='10px'
+         size ='md'
+         />
+        </div>
             </div>
+            
           ))}
 
         </div>
@@ -76,22 +63,6 @@ const Ecommerce = () => {
       <div className='flex gap-10 flex-wrap justify-center'>
         <div className='bg-white dark:text-gray-200 
         dark:bg-secondary-dark-bg m-3 rounded-2xl md:w-780'>
-          <div className='flex justify-between'>
-            <p className='font-semibold text-xl'>Revenue Updates</p>
-            <div className='flex items-center gap-2'>
-              <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
-                <span>< GoDot/>
-                <span>Expenses</span>
-                </span>
-                </p>
-
-                 <p className='flex items-center gap-2 text-green-600 hover:drop-shadow-xl'>
-                <span>< GoDot/>
-                <span>Budget</span>
-                </span>
-                </p>
-            </div>
-          </div>
 
           <div className='mt-10 flex gap flex-wrap justify-center'>
             <div className='border-r-1 border-color m-4 pr-10'>
@@ -116,14 +87,6 @@ const Ecommerce = () => {
               </div>
 
               <div className='mt-5'>
-               {/* <SparkLine
-                currentColor="blue"
-               id="line-sparkline"
-               type="Line"
-               height="80px"
-               width="250px"
-               data={SparklineAreaData}
-               color="blue" /> */}
 
                <SparkLine
   data={[{ x: 1, y: 10 }, { x: 2, y: 20 }, { x: 3, y: 15 }]}
@@ -143,11 +106,6 @@ const Ecommerce = () => {
               </div>
             </div>
 
-            {/* <div>
-              <Stacked 
-              width="320px"
-              height="360px"/>
-            </div> */}
           </div>
         </div>
       </div>
