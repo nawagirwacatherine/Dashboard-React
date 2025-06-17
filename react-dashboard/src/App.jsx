7e5013a2d2ec9ff9490ee4100d2299a903a82b59
Nawagirwa catherine <134449186+nawagirwacatherine@users.noreply.github.com>
2025-06-17 10:22:44 +0200

    correct page rendering on small screens

--- react-dashboard/src/App.jsx
+++ react-dashboard/src/App.jsx
@@ -34,7 +34,7 @@ const App = () => {
 
       
         {activeMenu ? (
-          <div className='w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white'>
+          <div className='w-72 fixed  z-50 sidebar dark:bg-secondary-dark-bg bg-white'>
            < Sidebar/>
           </div>
         ):(
